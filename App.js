3c9d35a30ede31b774f5841391cc2cb9042aeb90
Aleksandr-Pod <stdfire@gmail.com>
2022-06-10 19:13:29 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ console.clear();
 // const { userOne, userTwo } = require('./testFolder');
 // console.log("Result:", userTwo);
 
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
+//          или
+const fs = require('fs/promises');
 // fs.appendFile('testFolder/index.js', '// пробую добавить строку в файл');
 // fs.readFile('testFolder/index.js').then(data => console.log(data.toString()));
 const path = require('path');
 const allPath = path.join(__dirname, "testFolder");
 console.log('allPath:', allPath);
-
+//     с этим (allPath) не работает,
+//     но работает с (__dirname)
 fs.readdir(allPath)
   .then(files => {
     return Promise.all(
@@ -23,3 +26,8 @@ fs.readdir(allPath)
     );
   })
   .then(result => console.table(result));
+  
+//     а тут работает и так, и так
+fs.readFile(`${allPath}/index.js`)
+  .then(res => console.log(res.toString()))
+  .catch(err => console.log(err.message))
